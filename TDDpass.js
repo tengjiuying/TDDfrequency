d5a8c28a9d8e5c2bf5cca9d93037c95b1eac49f3
TengFeng2017 <2940681539@qq.com>
2017-05-18 21:44:24 +0800

    TDDpass.js提交

--- TDDpass.js
+++ TDDpass.js
@@ -1,16 +1,43 @@
 /*** Created by think；pad on 2017/5/18.*/
-var = require("lodash");
-var chai =require("chai");
-var sinon =require("sinon");
-var sinonChai = require("sinon-chai");
-var expect = require("sinon-chai");
-chai.use(sinonChai);
-
-var main = require("../lib/main.js");
-
-describe("World Frequency",function(){
-    it("returns empty string given empty string ", function(){
+describe("Word Frequency", function () {
+    it("returns empty string given empty string", function () {
         var result = main('');
         expect(result).toEqual('');
+
     });
-});
+
+    it("returns string given one word", function () {
+        var result = main('he');
+        expect(result).toEqual('he 1');
+
+    });
+
+    it("returns string given two different word", function () {
+
+        var result = main('he is');
+        expect(result).toEqual('he 1\r\nis 1');
+
+    });
+
+    it("returns string given two duplicated word", function () {
+        var result = main('he is he');
+        expect(result).toEqual('he 2\r\nis 1');
+
+    });
+
+    it("returns string given two duplicated words need to be sorted", function () {
+        var result = main('he is is');
+        expect(result).toEqual('is 2\r\nhe 1');
+
+    });
+
+    it("returns string given two duplicated words splited by multiple spaces", function () {
+        var result = main('he     is');
+        expect(result).toEqual('he 1\r\nis 1');
+
+    });
+    it("returns string given long string splited by multiple spaces", function () {
+        var result = main('it was the age of wisdom it was the age of foolishness it is');
+        expect(result).toEqual('it 3\r\nwas 2\r\nthe 2\r\nage 2\r\nof 2\r\nwisdom 1\r\nfoolishness 1\r\nis 1');
+        document.write("result:"+"<br>"+result);
+    });
