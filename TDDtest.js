c3215ed86b6ea7f3101195bdd524bd9dc696e841
TengFeng2017 <2940681539@qq.com>
2017-05-18 21:43:34 +0800

    TDDtest.js提交

--- TDDtest.js
+++ TDDtest.js
@@ -1,7 +1,38 @@
 /**
  * Created by think；pad on 2017/5/18.
  */
-function main(){
+var formatWordAndCount = function (word, count) {
+    return word +
+        ' ' +
+        count;
+};
+var group = function (wordArray) {
+    return wordArray.reduce((array,word)=>{
+            let entry = array.find((e)=> e.word === word);//找对象
+    if(entry){
+        entry.count++;
+    }else{
+        array.push({word:word , count:1});
+    }
+    return array;
+},[]);
+};
+var split = function (words) {
+    return words.split(/\s+/);
+};
+var sort = function (groupWords) {
+    groupWords.sort((x, y) => y.count - x.count);
+};
+
+function format(groupWords) {
+    return groupWords.map((e) = > formatWordAndCount(e.word, e.count)).join('\r\n');
+}
+function main(words){
+    if(words !== ''){
+        let wordArray=split(words);
+        let groupWords = group(wordArray);
+        sort(groupWords);
+        return format(groupWords);
+    }
     return ''
 }
-module.TDDtest = main;
